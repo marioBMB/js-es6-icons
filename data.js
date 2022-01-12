09bdb7ea2a8547556a469f3ddb2632dbeef1b7c4
marioBMB <m.sannicola@studenti.uniba.it>
2022-01-12 17:04:31 +0100

    problemi nel change della select

---
+++ data.js
@@ -0,0 +1,158 @@
+let icons = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	}
+];
+
+
+
+const iconsBox = document.getElementById("icons-container");
+const selectType = document.getElementById("type-filter");
+
+
+selectType.addEventListener("change", function(){
+	let type = selectType.options[selectType.selectedIndex].value;
+	renderIconBoxes(type);
+});
+
+
+function renderIconBoxes(iconType){
+
+	
+	html = "";
+	
+	if (iconType != ""){
+		
+		icons = icons.filter((item) => {
+			return iconType === item.type;
+		});
+	}
+
+	icons.forEach((item) => {
+		
+		html += getIconTemplate(item);
+	});
+
+	iconsBox.innerHTML = html;
+}
+
+
+function getIconTemplate(postData){
+
+	const { name, prefix, type, family, color } = postData;
+
+	return `<div class="box">
+				<i class="${family} ${prefix}${name}" style='color: ${color}'></i>
+				<div class="title">${name}</div>
+			</div>`;
+}
+
